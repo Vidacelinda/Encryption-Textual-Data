2be1e0baa29293e1fd3d374d387f714675866ddf
Carlo Leiva <87499194+Vidacelinda@users.noreply.github.com>
2023-11-09 07:50:08 -0500

    added files
    added file keys to store keys. added excel_test_file

--- symmetric_encryption.py
+++ symmetric_encryption.py
@@ -9,15 +9,27 @@ step 4 - decript key
 """
 
 
-key = Fernet.generate_key()
+# key = Fernet.generate_key()
 #TEST print("encripted key", key)
-cipher = Fernet(key)
+
+# #WRITE key to file to save
+# fkey=open("file_key.text",'wb')
+# fkey.write(key)
+
+#READ key from file
+fkey=open("file_key.text",'rb')
+key=fkey.read()
+cipher=Fernet(key)
+print(key)
+
+
 #ENCRPTION
-encrypted_text=cipher.encrypt(b'this is my secret message')
-print(encrypted_text)
+encrypted_text=cipher.encrypt(b't1 this is my secret message')
+#.decode() removes -> b''
+# print(encrypted_text.decode())# ex: $dg#56
 
 #DECRIPTION
 orignal_text=cipher.decrypt(encrypted_text)
-print(orignal_text.decoded())
+print(orignal_text.decode())# ex: hello
 
 # cipher= Ferent(key)
