fb7870c146869eccc02ea76fcde71c0622ee9bc0
Carlo Leiva <87499194+Vidacelinda@users.noreply.github.com>
2023-12-02 12:11:35 -0500

    cleaning code ( refactoring )
    added better instructions on how the code works. (more still needs to be added )

--- symmetric_decryption.py
+++ symmetric_decryption.py
@@ -1,16 +1,35 @@
 from cryptography.fernet import Fernet
 
-# open saved key file
-def decrypt(file):
-    fkey = open("file_key.text", 'rb')
-    key=fkey.read()
-    # use fernet scheme
-    cipher=Fernet(key)
-    # read saved encrypted_data
-    with open('excel_test_file.xlsx_encrypted','rb') as df:
-        encrypted_data=df.read()
-    # Decrypt the encrypted_data to obtain the original data.
-    decrypted_file=cipher.decrypt(encrypted_data)
-    # make a new file showing the decrpted file
-    with open('decrypted_excel_test_file.xlsx','wb') as df:
-        df.write(decrypted_file)
+
+# CODE FOR STUDYING (IGNORE)
+""" TEST 1 / Testing Plain text / cons : does not save key and has to be regenerated """
+
+key = Fernet.generate_key()
+# TEST print("encripted key", key)
+
+#symetrical encrption
+cipher=Fernet(key)
+#ENCRPTION
+secret_str='t1 this is my secret message'
+print('secret message: ',secret_str)
+# encrypt // note
+encrypted_text=cipher.encrypt(secret_str.encode())
+#.decode() removes -> b''
+print(encrypted_text.decode())# ex: $dg#56
+
+#DECRIPTION
+orignal_text=cipher.decrypt(encrypted_text)
+print(orignal_text.decode()) # ex: hello
+
+# cipher= Ferent(key)
+'''
+
+
+"""TEST 2 / generate Key and save to file to be used and dycrypted later"""
+'''
+key = Fernet.generate_key()
+# TEST print("encripted key", key)
+
+# WRITE key to file to save
+fkey = open("file_key.text", 'wb')
+fkey.write(key)
--- symmetric_encryption.py
+++ symmetric_encryption.py
@@ -1,14 +1,14 @@
 from cryptography.fernet import Fernet
-"""
-symmetric encryption
-note - Libary used cryptography.fernet
-step 1 - encript
-step 2 - file write the encripted key
-step 3 - read from file encripted key
-step 4 - decript key
-"""
 
+"""
+1- generate key
+2- save generated key to "file_key.text"
 
+Encryption 
+3- read key form "file_key.text"
+4- use key for the cipher algorithm using Fernet/ ex: cipher=Fernet(key)
+5- 
+"""
 
 
 """Generate file to hold key"""
@@ -73,40 +73,3 @@ if __name__ == '__main__':
 
 
 
-
-
-
-
-# CODE FOR STUDYING (IGNORE)
-""" TEST 1 / Testing Plain text / cons : does not save key and has to be regenerated """
-'''
-key = Fernet.generate_key()
-# TEST print("encripted key", key)
-
-#symetrical encrption
-cipher=Fernet(key)
-#ENCRPTION
-secret_str='t1 this is my secret message'
-print('secret message: ',secret_str)
-# encrypt // note 
-encrypted_text=cipher.encrypt(secret_str.encode())
-#.decode() removes -> b''
-print(encrypted_text.decode())# ex: $dg#56
-
-#DECRIPTION
-orignal_text=cipher.decrypt(encrypted_text)
-print(orignal_text.decode()) # ex: hello
-
-# cipher= Ferent(key)
-'''
-
-
-"""TEST 2 / generate Key and save to file to be used and dycrypted later"""
-'''
-key = Fernet.generate_key()
-# TEST print("encripted key", key)
-
-# WRITE key to file to save
-fkey = open("file_key.text", 'wb')
-fkey.write(key)
-'''
