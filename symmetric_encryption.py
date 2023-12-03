ff5af5e07a8cc78a3c95d822fd9b88c2413caa8a
Carlo Leiva <87499194+Vidacelinda@users.noreply.github.com>
2023-12-03 15:07:00 -0500

    code fix (Refactoring)

--- symmetric_encryption.py
+++ symmetric_encryption.py
@@ -1,18 +1,30 @@
 from cryptography.fernet import Fernet
 
 """
+FUNCTIONS 
+
+generate_key function
 1- generate key
 2- save generated key to "file_key.text"
 
-Encryption 
+Encryption function
 3- read key form "file_key.text"
 4- use key for the cipher algorithm using Fernet/ ex: cipher=Fernet(key)
-5 -
+5- open the file you want to encypt .Encrypt said file using the cipher algorithm
+summary - encrypts file and produces cipher text file ("encrypted_excel_test_file.xlsx.text")
+
+Decryption function
+7- read key from file "file_key_text" 
+8- use key with Fernet('key') for the cipher algorithm
+9- read encrypted file then decrypted using cipher.dercypt('file')
+10- write decrypted data to a file for visualization.
+
+
 """
 
 
 """Generate file to hold key"""
-def generate_file():
+def generate_key():
     # generate key
     key = Fernet.generate_key()
     print("encripted key : ", key)
@@ -42,18 +54,19 @@ def encrypt_file(file):
     with open("encrypted_"+file_name+".text",'wb') as ef:
         ef.write(encrypted_file)
 
-# open saved key file
+""" Decrypt file /symmetric decryption """
 def decrypt_file(file):
+    # open saved key file
     fkey = open("file_key.text", 'rb')
     key=fkey.read()
     # use fernet scheme
     cipher=Fernet(key)
     # read saved encrypted_data
-    with open('encrypted_excel_test_file.xlsx.text','rb') as df:
+    with open(file,'rb') as df:
         encrypted_data=df.read()
     # Decrypt the encrypted_data to obtain the original data.
     decrypted_file=cipher.decrypt(encrypted_data)
-    # make a new file showing the decrpted file
+    # make a new file showing the decrypted file
     with open('decrypted_excel_test_file.xlsx','wb') as df:
         df.write(decrypted_file)
 
@@ -61,10 +74,10 @@ def decrypt_file(file):
 def main():
     print("Main function")
     if (input('generate key ? enter y/n ').lower())=='y':
-        generate_file()
+        generate_key()
 
     encrypt_file('excel_test_file.xlsx')
-    decrypt_file('excel_test_file.xlsx')
+    decrypt_file('encrypted_excel_test_file.xlsx.text')
 
 
 if __name__ == '__main__':
