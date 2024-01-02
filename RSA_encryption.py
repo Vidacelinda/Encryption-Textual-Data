50f663357f60f6a70b4d4db36f3a1f26564e2b59
Carlo Leiva <87499194+Vidacelinda@users.noreply.github.com>
2024-01-01 23:02:45 -0500

    refactering  (small fix)

--- RSA_encryption.py
+++ RSA_encryption.py
@@ -2,7 +2,7 @@ import random
 
 def is_prime(number):
     if number<2:
-        return False:
+        return False
     for i in range(2,number // 2+1):
         if number % i== 0:
             return False
