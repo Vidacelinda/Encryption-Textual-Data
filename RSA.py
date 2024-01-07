6b921ec994910e11d3f1a79b56af1ef3a62651c4
Carlo Leiva <87499194+Vidacelinda@users.noreply.github.com>
2024-01-06 19:20:18 -0500

    refactoring (bug)  / added message to encrypt
    1. used path to public.pem when reading for the private by accident on line 24. simply change the path to find private.pem
    line 24
    2. show case the encryption working properly using the public key.

--- RSA.py
+++ RSA.py
@@ -1,21 +1,31 @@
 import rsa
 
 """generate a private ande public key and save """
-# # 1024 bit for testing / recomemended to use 2048 for secure RSA
+# 1024 bit for testing / recomemended to use 2048 for secure RSA
 # public_key,private_key=rsa.newkeys(1024)
+#
 # with open("RSA keys/public.pem","wb") as f:
+# # with open("public.pem","wb") as f:
 #     #save_pkcs1 algorithm
 #     f.write(public_key.save_pkcs1("PEM"))
 #
 # with open("RSA keys/private.pem","wb") as f:
+# # with open("private.pem","wb") as f:
 #     f.write(private_key.save_pkcs1("PEM"))
 
 
-"""once you have your generate keys you would want to reuse the keys"""
-public_key,private_key=rsa.newkeys(1024)
-with open("RSA keys/public.pem","wb") as f:
-    #when reading the pem format you would have to load it using the same algorithm used when saving they key to the file.
-    public_key =rsa.publicKey.load_pkcs1(f.read())
 
-with open("RSA keys/private.pem","wb") as f:
-    private_key =rsa.publicKey.load_pkcs1(f.read())
+"""once you have your generate keys you would want to reuse the keys"""
+with open("RSA keys/public.pem","rb") as f:
+# with open("public.pem", "rb") as f:
+    #when reading the pem format you would have to load it using the same algorithm used when saving they key to the file.
+    public_key = rsa.PublicKey.load_pkcs1(f.read())
+
+with open("RSA keys/private.pem","rb") as f:
+# with open("private.pem","rb") as f:
+    private_key = rsa.PrivateKey.load_pkcs1(f.read())
+
+message = "Taco please"
+
+encrypted_message = rsa.encrypt(message.encode(),public_key)
+print(encrypted_message)
