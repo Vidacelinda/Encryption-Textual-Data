74b3da31507a47c4d09f25c567f356e20a7beef3
Carlo Leiva <87499194+Vidacelinda@users.noreply.github.com>
2024-01-07 10:35:24 -0500

    decrypt message that was encrypted

--- RSA.py
+++ RSA.py
@@ -17,15 +17,24 @@ import rsa
 
 """once you have your generate keys you would want to reuse the keys"""
 with open("RSA keys/public.pem","rb") as f:
-# with open("public.pem", "rb") as f:
     #when reading the pem format you would have to load it using the same algorithm used when saving they key to the file.
     public_key = rsa.PublicKey.load_pkcs1(f.read())
 
 with open("RSA keys/private.pem","rb") as f:
-# with open("private.pem","rb") as f:
     private_key = rsa.PrivateKey.load_pkcs1(f.read())
 
-message = "Taco please"
+"""Test 1 : see if encrypted message works """
+# message = "Taco please"
+#
+# # encode() converts string into bytes
+# encrypted_message = rsa.encrypt(message.encode(),public_key)
+#
+# # print(encrypted_message)
+# with open("RSA encrypted message/encrypted.message", "wb") as f:
+#     f.write(encrypted_message)
 
-encrypted_message = rsa.encrypt(message.encode(),public_key)
-print(encrypted_message)
+
+encrypted_message = open("RSA encrypted message/encrypted.message", "rb").read()
+
+decrypted_message = rsa.decrypt(encrypted_message,private_key)
+print(decrypted_message)
